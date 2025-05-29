1d8f5a2c391779570f31d1096deebfe8362a54af
IvanYagovkin <159721004+IvanYagovkin@users.noreply.github.com>
2025-05-29 15:59:41 +0400

    Initializing the OLED Display

---
+++ Initializing the OLED Display.ino
@@ -0,0 +1,61 @@
+#include <GyverOLED.h>
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+
+const int maxInputLength = 64;
+char incomingData[maxInputLength];
+int dataIndex = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  oled.init(); // можно указать адрес: oled.init(0x3C) или oled.init(0x3D)
+  oled.clear();
+  oled.setScale(3);
+  oled.home();
+  oled.print("Привет!");
+
+  delay(1000);
+
+  oled.setScale(1);
+  oled.setCursor(0, 3);
+  oled.print("GyverOLED v1.3.2");
+
+  oled.setCursorXY(20, 50);
+  float pi = 3.14;
+  oled.print("PI = ");
+  oled.print(pi);
+
+  delay(2000);
+  oled.clear();
+
+  oled.setCursor(0, 0);
+  oled.print("Введите текст:");
+  oled.setCursor(0, 2);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+
+    if (c == '\n') {
+      incomingData[dataIndex] = '\0';
+
+      // Вывод в Serial для отладки
+      Serial.print("Вы ввели: ");
+      Serial.println(incomingData);
+
+      // Очистка и вывод на экран
+      oled.clear();
+      oled.setCursor(0, 0);
+      oled.print("Вы ввели:");
+      oled.setCursor(0, 2);
+      oled.print(incomingData);
+
+      dataIndex = 0;
+    } else {
+      if (dataIndex < maxInputLength - 1) {
+        incomingData[dataIndex++] = c;
+      }
+    }
+  }
+}
